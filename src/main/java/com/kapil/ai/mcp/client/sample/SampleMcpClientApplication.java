c73eae9b8b8840ad65b2b6724481d28c2c025493
kaplimonadi <kapilmonadi@gmail.com>
2026-04-17 16:45:14 +0530

    Initial commit

---
+++ src/main/java/com/kapil/ai/mcp/client/sample/SampleMcpClientApplication.java
@@ -0,0 +1,13 @@
+package com.kapil.ai.mcp.client.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMcpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMcpClientApplication.class, args);
+	}
+
+}
