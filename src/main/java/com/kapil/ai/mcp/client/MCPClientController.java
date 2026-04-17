432fe82535fcb2b78382da340a5795835ae4d5ac
kaplimonadi <kapilmonadi@gmail.com>
2026-04-17 17:36:21 +0530

    fixed windows related issue

---
+++ src/main/java/com/kapil/ai/mcp/client/MCPClientController.java
@@ -0,0 +1,29 @@
+package com.kapil.ai.mcp.client;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MCPClientController {
+    private  final ChatClient chatClient;
+
+    public MCPClientController(ChatClient.Builder chatClientBuilder,
+                               ToolCallbackProvider toolCallbackProvider) {
+        this.chatClient = chatClientBuilder.defaultToolCallbacks(toolCallbackProvider)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("message") String message) {
+        return chatClient.prompt()
+                .call().
+                content();
+    }
+}
